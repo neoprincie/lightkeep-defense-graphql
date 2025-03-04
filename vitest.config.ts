d92492e4ae6aa714942e874d4991fc9c1207f11a
Bryan Prince <bryanprince@Bryans-MacBook-Air.local>
2025-03-04 18:46:06 -0500

    update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      provider: 'istanbul'
+        provider: 'v8',
+        reporter: ['text', 'json-summary', 'json'],
+        reportOnFailure: true
     },
   },
 });
