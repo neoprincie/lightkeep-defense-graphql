867af51cba294975fb023dd2b0081bb0fa2ed27a
Bryan Prince <bryanprince@Bryans-MacBook-Air.local>
2025-03-30 14:16:12 -0400

    Update acceptance test to work with new functionality

--- acceptance/features/step_definitions/stepdefs.js
+++ acceptance/features/step_definitions/stepdefs.js
@@ -6,11 +6,13 @@ import crypto from 'crypto'
 
 Given('I am logged in as {string}', async function (userName) {
     this.userName = `${userName}${crypto.randomUUID()}`;
-    this.token = await registerUser(`${crypto.randomUUID()}@example.com`, userName, crypto.randomUUID());
+    const authPayload = await registerUser(`${crypto.randomUUID()}@example.com`, this.userName, crypto.randomUUID());
+    this.token = authPayload.token;
 });
 
-Given('I have a character named {string}', function (characterName) {
-    newCharacter("Napryn", "Warrior", this.userName, this.token);
+Given('I have a character named {string}', async function (characterName) {
+    this.characterName = `Napryn${crypto.randomUUID()}`
+    await newCharacter(this.characterName, "Warrior", this.userName, this.token);
 });
 
 When('I look up my characters', async function () {
@@ -18,7 +20,7 @@ When('I look up my characters', async function () {
 });
 
 Then('I see only characters that belong to me', function () {
-    assert.equal(this.characters[0].name, "Napryn");
+    assert.equal(this.characters[0].name, this.characterName);
     assert.equal(this.characters[0].level, 1);
     //assert.equal(this.characters[0].user.name, this.userName);
 });
